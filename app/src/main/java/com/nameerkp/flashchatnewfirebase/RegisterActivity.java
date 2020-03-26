8e0efc8c4d053f4104533aeedbad464ed0181b5e
nameer-kp <kpnameer@yahoo.com>
2020-03-26 13:53:30 +0530

    not finished

--- app/src/main/java/com/nameerkp/flashchatnewfirebase/RegisterActivity.java
+++ app/src/main/java/com/nameerkp/flashchatnewfirebase/RegisterActivity.java
@@ -1,12 +1,11 @@
-package com.londonappbrewery.flashchatnewfirebase;
+package com.nameerkp.flashchatnewfirebase;
+
 
 import android.app.AlertDialog;
-import android.content.Intent;
-import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
-import android.support.v7.app.AppCompatActivity;
 import android.text.TextUtils;
+import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.KeyEvent;
 import android.view.View;
@@ -14,9 +13,14 @@ import android.view.inputmethod.EditorInfo;
 import android.widget.AutoCompleteTextView;
 import android.widget.EditText;
 import android.widget.TextView;
-import android.widget.Toast;
 
 
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 
 public class RegisterActivity extends AppCompatActivity {
@@ -24,6 +28,7 @@ public class RegisterActivity extends AppCompatActivity {
     // Constants
     public static final String CHAT_PREFS = "ChatPrefs";
     public static final String DISPLAY_NAME_KEY = "username";
+    String TAG="FlashChat";
 
     // TODO: Add member variables here:
     // UI references.
@@ -33,7 +38,7 @@ public class RegisterActivity extends AppCompatActivity {
     private EditText mConfirmPasswordView;
 
     // Firebase instance variables
-
+    private FirebaseAuth mAuth;
 
 
     @Override
@@ -59,7 +64,7 @@ public class RegisterActivity extends AppCompatActivity {
         });
 
         // TODO: Get hold of an instance of FirebaseAuth
-
+        mAuth = FirebaseAuth.getInstance();
 
     }
 
@@ -105,6 +110,7 @@ public class RegisterActivity extends AppCompatActivity {
             focusView.requestFocus();
         } else {
             // TODO: Call create FirebaseUser() here
+            createFirebaseUser();
 
         }
     }
@@ -116,17 +122,43 @@ public class RegisterActivity extends AppCompatActivity {
 
     private boolean isPasswordValid(String password) {
         //TODO: Add own logic to check for a valid password (minimum 6 characters)
-        return true;
+        String confirmPassword = mConfirmPasswordView.getText().toString();
+        if (password.equals(confirmPassword) && password.length()>=6) {
+            return true;
+
+        }else
+        {
+            return false;
+        }
     }
-
     // TODO: Create a Firebase user
+    private void createFirebaseUser()
+    {
+        String email = mEmailView.getText().toString();
+        String password = mPasswordView.getText().toString();
+        mAuth.createUserWithEmailAndPassword(email, password).addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+                Log.d(TAG, "onComplete: successful");
+                if (!task.isSuccessful()) {
+                    Log.d(TAG, "onComplete: user Creation Failed");
+                    showErrorDialog("Registration attempt failed");
+                }
+            }
+        });
 
+
+    }
 
     // TODO: Save the display name to Shared Preferences
 
 
     // TODO: Create an alert dialog to show in case registration failed
+    private void showErrorDialog(String messagae){
 
+        new AlertDialog.Builder(this).setTitle("Error").setMessage(messagae).setPositiveButton(android.R.string.ok,null)
+                .setIcon(android.R.drawable.ic_dialog_alert).show();
+    }
 
 
 
